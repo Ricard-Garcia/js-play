5c3ea10b042410ae2678d35d0a3a65458ca8f79e
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-29 15:45:14 +0200

    Added comments

--- changePage.js
+++ changePage.js
@@ -1,46 +1,61 @@
 var logButton = document.getElementById("initialButton");
 logButton.addEventListener('click',userToGame);
 
+// Go to game page
 function userToGame() {
+    // Checking user form
     var form = document.getElementById('userNameForm');
     form.addEventListener("submit", function (event) {
         event.preventDefault();
+
+        // Remove previous left block
         let leftTemplate = document.getElementById('leftBlock');
         leftTemplate.innerHTML = "";
+
+        // Add next left block
         let gamePageTemplate = document.getElementById('gamePage');
         const gamePageTemplateContent = document.importNode(gamePageTemplate.content, true);
-        // let gamePageTemplateContent = gamePageTemplate.content;
         document.getElementById('leftBlock').appendChild(gamePageTemplateContent);
         var stopButton = document.getElementById("stopGame");
         stopButton.addEventListener('click', gameToResults);
     });
 
+    // Test print
     console.log("Switched to game")
 
 }
 
+// Go to results page
 function gameToResults() {
+    // Remove previous left block
     let leftTemplate = document.getElementById('leftBlock');
     leftTemplate.innerHTML = "";
+
+    // Add next left block
     let resultPageTemplate = document.getElementById('resultPage');
     const resultPageTemplateContent = document.importNode(resultPageTemplate.content, true);
-    // let resultPageTemplateContent = resultPageTemplate.content;
     document.getElementById('leftBlock').appendChild(resultPageTemplateContent);
     var againButton = document.getElementById("playAgain");
     againButton.onclick = resultsToUser;
 
+    // Test print
     console.log("Switched to results")
 
 }
 
+// Go to initial page
 function resultsToUser() {
+    // Remove previous left block
     let leftTemplate = document.getElementById('leftBlock');
     leftTemplate.innerHTML = "";
     let userPageTemplate = document.getElementById('userPage');
     const userPageTemplateContent = document.importNode(userPageTemplate.content, true);
-    // let userPageTemplateContent = userPageTemplate.content;
     document.getElementById('leftBlock').appendChild(userPageTemplateContent);
+
+    // Test print
     console.log("Switched to user");
+
+    //Reset user to game to default
     userToGame();
 
 
