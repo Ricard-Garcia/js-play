e5c9ccc9529097363ea9995695164cdaa2107d66
haykbit <hpetrosyan.projects@gmail.com>
2021-03-29 15:28:39 +0200

    Modified changepage.js

--- changePage.js
+++ changePage.js
@@ -1,5 +1,5 @@
-var Button = document.getElementById("initialButton");
-Button.onclick = userToGame;
+var logButton = document.getElementById("initialButton");
+logButton.addEventListener('click',userToGame);
 
 function userToGame() {
     var form = document.getElementById('userNameForm');
@@ -10,5 +10,26 @@ function userToGame() {
         let gamePageTemplate = document.getElementById('gamePage');
         let gamePageTemplateContent = gamePageTemplate.content;
         document.getElementById('leftBlock').appendChild(gamePageTemplateContent);
+        var stopButton = document.getElementById("stopGame");
+        stopButton.addEventListener('click', gameToResults);
     });
+}
+
+function gameToResults() {
+    let leftTemplate = document.getElementById('leftBlock');
+    leftTemplate.innerHTML = "";
+    let resultPageTemplate = document.getElementById('resultPage');
+    let resultPageTemplateContent = resultPageTemplate.content;
+    document.getElementById('leftBlock').appendChild(resultPageTemplateContent);
+    var againButton = document.getElementById("playAgain");
+    againButton.onclick = resultsToUser;
+}
+
+function resultsToUser() {
+    let leftTemplate = document.getElementById('leftBlock');
+    leftTemplate.innerHTML = "";
+    let userPageTemplate = document.getElementById('userPage');
+    let userPageTemplateContent = userPageTemplate.content;
+    document.getElementById('leftBlock').appendChild(userPageTemplateContent);
+
 }
