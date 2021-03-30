46be8656f1dd62e29856acc5fa9c3ee8615d9592
haykbit <hpetrosyan.projects@gmail.com>
2021-03-30 10:13:25 +0200

    Cleaned changePage.js

--- changePage.js
+++ changePage.js
@@ -1,8 +1,10 @@
-var logButton = document.getElementById("initialButton");
-logButton.addEventListener("click", userToGame);
-
 var player_name = "";
 var scoreArray = [];
+var startTime = 0;
+var stopTime = 0;
+var result = 0;
+var logButton = document.getElementById("initialButton");
+logButton.onclick = userToGame;
 
 // Go to game page
 function userToGame() {
@@ -10,118 +12,86 @@ function userToGame() {
   var form = document.getElementById("userNameForm");
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    player_name = document.querySelector("form#userNameForm input").value;
-    console.log(player_name);
-
+    player_name = document.getElementById('userInput').value;
     // Remove previous left block
     let leftTemplate = document.getElementById("leftBlock");
     leftTemplate.innerHTML = "";
-
     // Add next left block
     let gamePageTemplate = document.getElementById("gamePage");
-    const gamePageTemplateContent = document.importNode(
-      gamePageTemplate.content,
-      true
-    );
-    document.getElementById("leftBlock").appendChild(gamePageTemplateContent);
+    const gamePageTemplateContent = document.importNode(gamePageTemplate.content, true);
+    leftTemplate.appendChild(gamePageTemplateContent);
 
     document.getElementById("getReady").style.display = "none";
     //_______start
     var startButton = document.getElementById("startGame");
-    startButton.addEventListener("click", showGetReady);
+    startButton.onclick = showGetReady;
   });
-  /* //_______stop
-     var stopButton = document.getElementById("stopGame");
-        
-     stopButton.addEventListener('click', gameToResults);*/
+  //_______stop
   function showGetReady() {
-    document.getElementById("getReady").style.display = "";
+    var randomNumber = Math.floor(Math.random() * 10000) + 1000;
+    var getReadyText = document.getElementById("getReady");
+    getReadyText.style.display = "";
     document.getElementById("startGame").style.display = "none";
-
     setTimeout(function () {
-      document.getElementById("getReady").style.display = "none";
+      getReadyText.style.display = "none";
       showStop();
-    }, 1000);
-    // Add next left block
-  }
-
-  // Test print
-  console.log("Switched to game");
-}
+    }, randomNumber);
+  };
+};
 // _________________In this global variables we set the times we need.
-var go = 0;
-var end = 0;
-var result = 0;
-console.log("old value of result", result);
 function showStop() {
-  go = Date.now();
+  startTime = Date.now();
   // Add next left block
   let stopPageTemplate = document.getElementById("stopPage");
-  const stopPageTemplateContent = document.importNode(
-    stopPageTemplate.content,
-    true
-  );
+  const stopPageTemplateContent = document.importNode(stopPageTemplate.content, true);
   document.getElementById("leftBlock").appendChild(stopPageTemplateContent);
-
   // _________________Giving funcionality to stop button.
+  document.getElementById("stopGame").addEventListener("click", finish);
+  // _________________calculating the seconds.
+  function finish() {
+    stopTime = Date.now();
+    result = (stopTime - startTime) / 1000;
+    result = result.toFixed(2);
+    console.log(result);
+    // _________________finally we set the values in a localStorage.
+    localStorage.setItem(player_name, result);
+    console.log(localStorage);
 
-  document.getElementById("stopGame").addEventListener(
-    "click",
-
-    // _________________calculating the seconds.
-
-    function finish() {
-      end = Date.now();
-      result = (end - go) / 1000;
-      result = result.toFixed(2);
-      console.log(result);
-      // _________________finally we set the values in a localStorage.
-      localStorage.setItem(player_name, result);
-      console.log(localStorage);
-
-      // sidebar top users and scores
-      for (var i = 0; i < localStorage.length; i++) {
-        userName = localStorage.key(i);
-        userScore = localStorage.getItem(userName);
-        scoreArray.push({ username: userName, score: userScore });
-      }
-      var topFive = scoreArray.sort((a, b) => a.score - b.score).slice(0, 5);
-
-      var topPlayers = document.querySelectorAll(".topName");
-      var topScores = document.querySelectorAll(".topScore");
-
-      for (var i = 0; i < topFive.length; i++) {
-        topPlayers[i].textContent = topFive[i].username;
-        topScores[i].textContent = `${topFive[i].score} seconds`;
-      }
-      gameToResults();
+    // sidebar top users and scores
+    for (var i = 0; i < localStorage.length; i++) {
+      userName = localStorage.key(i);
+      userScore = localStorage.getItem(userName);
+      scoreArray.push({ username: userName, score: userScore });
     }
-  );
-}
+    var topFive = scoreArray.sort((a, b) => a.score - b.score).slice(0, 5);
 
+    var topPlayers = document.querySelectorAll(".topName");
+    var topScores = document.querySelectorAll(".topScore");
+
+    for (var i = 0; i < topFive.length; i++) {
+      topPlayers[i].textContent = topFive[i].username;
+      topScores[i].textContent = `${topFive[i].score} seconds`;
+    }
+    gameToResults();
+  }
+};
 // Go to results page
 function gameToResults() {
   // Remove previous left block
   let leftTemplate = document.getElementById("leftBlock");
   leftTemplate.innerHTML = "";
-
   // Add next left block
   let resultPageTemplate = document.getElementById("resultPage");
-  const resultPageTemplateContent = document.importNode(
-    resultPageTemplate.content,
-    true
-  );
-  document.getElementById("leftBlock").appendChild(resultPageTemplateContent);
+  const resultPageTemplateContent = document.importNode(resultPageTemplate.content,true);
+  leftTemplate.appendChild(resultPageTemplateContent);
+
+  var secondsScore = document.getElementById("secondsScore");
+  secondsScore.innerHTML = `${result} seconds`;
   var againButton = document.getElementById("playAgain");
   againButton.onclick = resultsToUser;
-
-  var secondScore = document.querySelector("#secondScore");
-  //var currentScore = Object.values(localStorage).pop();
-  secondScore.innerHTML = `${result} seconds`;
   // Test print
   console.log("Switched to results");
-}
-
+};
 // Go to initial page
 function resultsToUser() {
   console.log(player_name, result, "result");
@@ -129,15 +99,8 @@ function resultsToUser() {
   let leftTemplate = document.getElementById("leftBlock");
   leftTemplate.innerHTML = "";
   let userPageTemplate = document.getElementById("userPage");
-  const userPageTemplateContent = document.importNode(
-    userPageTemplate.content,
-    true
-  );
-  document.getElementById("leftBlock").appendChild(userPageTemplateContent);
-
-  // Test print
-  console.log("Switched to user");
-
+  const userPageTemplateContent = document.importNode(userPageTemplate.content,true);
+  leftTemplate.appendChild(userPageTemplateContent);
   //Reset user to game to default
   userToGame();
-}
+};
