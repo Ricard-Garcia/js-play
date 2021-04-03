a99b02297d0d2d9eac9dfa36aa3a5c735bbab4f5
jonCroatanUto <jon.garciaorad.asencor@gmail>
2021-04-03 18:26:17 +0200

    transiciones

--- changePage.js
+++ changePage.js
@@ -28,35 +28,35 @@ function userToGame() {
     //_______start
     var startButton = document.getElementById("startGame");
     startButton.onclick = showGetReady;
-      //_______________________________________animation,hidde slide and fullscreen game
+      //__________________________animation,hidde slide and fullscreen game
 
-      document.getElementById("sideBar").style.transform = "scaleX(0.5) scaleY(0.5) matrix(1, 0, 1, 1,  100, 250) skewX(90deg) rotateZ(90deg) translateX(500px)";
-      document.getElementById("sideBar").style.opacity='0' 
-      document.getElementById("leftBlock").style.transform = " translateX(40%) ";
-      document.querySelector('body').style.background="linear-gradient(75deg, #142949, #274e45, #74752a, #9f1818)";
       
+      document.getElementById("sideBar").style.animationName='slideAnimation'; //This is another way: document.getElementById("sideBar").style.transform = "scaleX(0.5) scaleY(0.5) matrix(1, 0, 1, 1,  100, 250) translateX(500px)";
+      document.getElementById("sideBar").style.animationDuration='0.5s';
+      document.getElementById("sideBar").style.animationDirection='normal';
+      //__________________________startButton apears
+      startButton.style.animationName='startButon';
       
-      
-          
-      
-          setTimeout(function () {
-            startButton.style.transition=" 1s";
-            startButton.style.fontSize="20px";
-            startButton.style.width='300px';
+            /* ____________'Proces'befor using animations @kyframes
 
-          }, 100);
-      
-      //document.getElementById("leftBlock").style.backgroundColor="blue";
-      
-      
-        //document.getElementById("sideBar").style.display='none';
+            leftTemplate.style.transform = " translateX(40%) ";
+            document.querySelector('body').style.background="linear-gradient(75deg, #142949, #274e45, #74752a, #9f1818)";
+              setTimeout(function () {
+                  
+                  startButton.style.fontSize="20px";
+                  startButton.style.width='300px';
+                  startButton.style.transform='translateX(400px)';
+                  startButton.style.transition='1s;'
+
+                }, 200);
+            document.getElementById("leftBlock").style.backgroundColor="blue";
+            document.getElementById("sideBar").style.display='none';
+            _________________________________*/
       
        setTimeout(function () {
           document.getElementById("sideBar").style.display = "none";
-          document.getElementById("mainSection").style.gridTemplateColumns="1fr ";
-          
-          
-        }, 500);
+         
+          }, 500);
       
     //________________________________________________________________________________
   });
@@ -64,7 +64,7 @@ function userToGame() {
   //_______stop
   function showGetReady() {
     
-    
+    document.getElementById("mainSection").style.gridTemplateColumns="1fr ";
     var randomNumber = Math.floor(Math.random() * 1000) + 1000;
     var getReadyText = document.getElementById("getReady");
     getReadyText.style.display = "";
@@ -83,7 +83,7 @@ function userToGame() {
 // _________________In this global variables we set the times we need.
 function showStop() {
   startTime = Date.now();
-    
+  
   // Add next left block
   let stopPageTemplate = document.getElementById("stopPage");
   const stopPageTemplateContent = document.importNode(stopPageTemplate.content, true);
@@ -95,7 +95,11 @@ function showStop() {
   stopButton.addEventListener("click", finish);
   // _________________calculating the seconds.
   function finish() {
+    //________________________________invert SlideAnimation, and bring it back
+    document.getElementById("sideBar").style.animationDirection='reverse';
     document.getElementById("sideBar").style.display='flex';
+    document.getElementById("mainSection").style.gridTemplateColumns="3fr 1fr";
+    //____________________________________________________________
     stopTime = Date.now();
     result = (stopTime - startTime) / 1000;
     result = result.toFixed(2);
@@ -119,23 +123,14 @@ function showStop() {
       topPlayers[i].textContent = `#${i+1} ` + topFive[i].username;
       topScores[i].textContent = `${topFive[i].score} seconds`;
     }
+    
     gameToResults();
   }
 };
 // Go to results page
 function gameToResults() {
 
-  //___________________________________invert elements animation
-  document.getElementById("sideBar").style.display = "flex";
-  document.getElementById("mainSection").style.gridTemplateColumns="3fr 1fr";
-  setTimeout(function () {
-      document.getElementById("sideBar").style.transform = "";
-      document.getElementById("sideBar").style.opacity='100' 
-      document.getElementById("leftBlock").style.transform = "";
-      
-  },500) ; 
 
-  //____________________________________________________________
   // Remove previous left block
   let leftTemplate = document.getElementById("leftBlock");
   leftTemplate.innerHTML = "";
@@ -153,6 +148,9 @@ function gameToResults() {
 };
 // Go to initial page
 function resultsToUser() {
+  
+      
+ ;
   console.log(player_name, result, "result");
   // Remove previous left block
   let leftTemplate = document.getElementById("leftBlock");
@@ -160,6 +158,14 @@ function resultsToUser() {
   let userPageTemplate = document.getElementById("userPage");
   const userPageTemplateContent = document.importNode(userPageTemplate.content,true);
   leftTemplate.appendChild(userPageTemplateContent);
+
+  //__________________________Repeat WelcomeAnimations
+     document.querySelector('.title-page').style.animationName='start';
+     document.querySelector('div#mainPageForm').style.animationName='inputName';
+           //Restart the slide animation
+     document.getElementById("sideBar").style.animationName=''; 
+  //________________________________________________ 
+
   //Reset user to game to default
   userToGame();
 };
