3c75803370ab8d90c1c0adfc0182cc19fed87c7a
haykbit <hpetrosyan.projects@gmail.com>
2021-04-06 11:39:45 +0200

    Configuration of levels

--- gameGrid.js
+++ gameGrid.js
@@ -47,16 +47,28 @@ function showGameGrid(gridSize, timeSpeed, repeatGrid) {
 };showGameGrid(60,1000,"repeat(5, 1fr)");
 
 var grid = document.getElementById("radioButton");
-var time = document.getElementById("radioButton");
 var grid1 = document.getElementById("radioButton1");
+var grid2 = document.getElementById("radioButton2");
+var time = document.getElementById("radioButton");
+var time1 = document.getElementById("radioButton1");
 var time2 = document.getElementById("radioButton2");
 
 if (grid.checked && time.checked){
   showGameGrid(12,1000,"repeat(3, 1fr)");
-} else if (grid1.checked && time2.checked) {
-  showGameGrid(60,1000,"repeat(5, 1fr)");
+} else if (grid.checked && time1.checked) {
+  showGameGrid(12,800,"repeat(3, 1fr)");
 } else if (grid.checked && time2.checked) {
-  showGameGrid(12,1000,"repeat(3, 1fr)");
+  showGameGrid(12,500,"repeat(3, 1fr)");
 } else if (grid1.checked && time.checked) {
+  showGameGrid(24,1000,"repeat(5, 1fr)");
+} else if (grid1.checked && time1.checked) {
+  showGameGrid(24,800,"repeat(5, 1fr)");
+} else if (grid1.checked && time2.checked) {
+  showGameGrid(24,500,"repeat(5, 1fr)");
+} else if (grid2.checked && time.checked) {
   showGameGrid(60,1000,"repeat(5, 1fr)");
-}
+} else if (grid2.checked && time1.checked) {
+  showGameGrid(60,800,"repeat(5, 1fr)");
+} else if (grid2.checked && time2.checked) {
+  showGameGrid(60,500,"repeat(5, 1fr)");
+} 
